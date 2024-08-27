7056826d26c2d6a2c83a7487b0190b6c46db7524
DJGAVE <djgaveking@outlook.com>
2024-08-27 02:16:58 +0100

    omiited apicontroller

--- routes/apiCaResults.js
+++ routes/apiCaResults.js
@@ -14,8 +14,8 @@ const apiController = require('../controllers/apiCaResultsController');
 
 router.get('/caresults', getCaResults);
 router.get('/caresults/:termid:sessionId', getCaResultsByTermIdSessionId);
-router.post('/caresults', apiController.createCaResult);
-router.put('/caresults/:Id', apiController.updateCaResult);
+router.post('/caresults', createCaResult);
+router.put('/caresults/:Id',updateCaResult);
 router.delete('/caresults/:Id', apiController.deleteCaResult);
 
 module.exports = router;
